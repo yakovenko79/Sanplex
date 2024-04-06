0daab3f6e1f131accf58e9350f2d8994648f70a6
MIKHAIL IAKOVENKO <114107429+yakovenko79@users.noreply.github.com>
2024-04-07 02:11:56 +0300

    fill some fields in the create project form (name, description, click on every radiobutton) Also creating conftest.py and package for tests

--- Sanplex_project/test_create_project.py
+++ Sanplex_project/test_create_project.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.expected_conditions import visibility_of_element
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def test_login():
+def test_create_project():
     driver = webdriver.Chrome()
     driver.set_window_size(1920, 1080)
     # time.sleep(1)
@@ -36,21 +36,19 @@ def test_login():
 
     driver.switch_to.frame(driver.find_element(By.CSS_SELECTOR, "iframe#appIframe-project"))
     create_project_btn = driver.find_element(By.CLASS_NAME, "create-project-btn")
-    # create_proj = driver.find_element()
-
-
     create_project_btn.click()
-
     time.sleep(3)
-
-    project_name_field = driver.find_element(By.CSS_SELECTOR, "textarea")
+    project_name_field = driver.find_element(By.CSS_SELECTOR, "textarea.is-required")
     project_name_field.send_keys(PROJECT_NAME)
-
-    description_frame = driver.find_element(By.CLASS_NAME, ".show-desc")
+    time.sleep(5)
+    description_frame = driver.find_element(By.CLASS_NAME, "show-desc")
     description_frame.click()
-
-    text_description = driver.find_element(By.CSS_SELECTOR, "zen-editor-content")
-    text_description.send_keys("test description")
-
-    radio_btn = driver.find_element(By.CSS_SELECTOR, "[for = multiple1]")
-    radio_btn.click()
+    time.sleep(3)
+    ActionChains(driver).move_to_element(driver.find_element(By.CSS_SELECTOR, "zen-editor")).click().send_keys("test_description").perform()
+    time.sleep(3)
+    radio_btn_0 = driver.find_element(By.CSS_SELECTOR, "[for = multiple_0]")
+    radio_btn_0.click()
+    time.sleep(3)
+    radio_btn_1 = driver.find_element(By.CSS_SELECTOR, "[for = multiple1]")
+    radio_btn_1.click()
+    time.sleep(3)
