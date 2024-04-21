dfd144c80579b643cc4e5138cd310d0b018e1efd
Ksenia Muntian <nimchinova.kseniya@gmail.com>
2024-04-21 18:03:08 +0300

    began coding CRUD Task test

--- Sanplex_project/test_crud_task.py
+++ Sanplex_project/test_crud_task.py
@@ -1,15 +1,19 @@
+import time
+
 from Sanplex_project.pages.base_page import BasePage
 from Sanplex_project.pages.dashboard_page import SideBar
 
 USER = "MTI_group7"
 PASSWORD = "HqHdMaHf6ZL737W"
-LINK = "http://mentorpiece.sanplex.com"
+LINK = "https://mentorpiece.sanplex.com/my-index.html"
+
 
 class TestCRUDTask:
 
-    def test_create_task(self,browser):
+    def test_create_task(self, browser):
         page = BasePage(browser, LINK)
         page.open()
         page.login(USER, PASSWORD)
-        side = SideBar(browser,browser.current_url)
+        side = SideBar(browser, browser.current_url)
+        time.sleep(3)
         side.go_to_work_dropdown_menu()
