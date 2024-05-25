fa9dda80cfefb514682965b6ee36d8837bbb1d1d
MIKHAIL IAKOVENKO <114107429+yakovenko79@users.noreply.github.com>
2024-05-25 16:58:04 +0300

    updated

--- Sanplex_project/test_crud_bugs.py
+++ Sanplex_project/test_crud_bugs.py
@@ -1,3 +1,4 @@
+import pytest
 
 from Sanplex_project.pages.base_page import BasePage
 from Sanplex_project.pages.bugs_page import BugsPage
@@ -10,30 +11,35 @@ LINK = "http://mentorpiece.sanplex.com"
 
 
 class TestCRUDBugs:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        self.page = BasePage(browser, LINK)
+        self.page.open()
+        self.page.login(USER, PASSWORD)
+        self.project_page = ProjectPage(browser, browser.current_url)
+        self.project_page.should_this_project_page()
+        self.sidebar = SideBar(browser, browser.current_url)
+        self.sidebar.go_to_projects_list()
+        self.sidebar.go_to_project()
+        self.sidebar.should_title_project_correct()
+        self.sidebar.go_to_bugs_page()
+        self.bugs_page = BugsPage(browser, browser.current_url)
+        self.bugs_page.is_this_bug_page()
 
     def test_create_bug(self, browser):
-        page = BasePage(browser, LINK)
-        page.open()
-        page.login(USER, PASSWORD)
-        project_page = ProjectPage(browser, browser.current_url)
-        project_page.should_this_project_page()
-        sidebar = SideBar(browser, browser.current_url)
-        sidebar.go_to_projects_list()
-        sidebar.go_to_project()
-        sidebar.should_title_project_correct()
-        sidebar.go_to_bugs_page()
+        """Test create bug"""
         bugs_page = BugsPage(browser, browser.current_url)
-        bugs_page.is_this_bug_page()
         bugs_page.create_new_bug()
         bugs_page.is_bug_present_in_the_bugs_list()
 
-
-
     def test_edit_bug(self, browser):
-        page = BasePage(browser, LINK)
-        page.open()
-        page.login(USER, PASSWORD)
-        project_page = ProjectPage(browser, browser.current_url)
-        project_page.should_this_project_page()
-        sidebar = SideBar(browser, browser.current_url)
+        """Test edit bug"""
+        bugs_page = BugsPage(browser, browser.current_url)
+        bugs_page.edit_bug()
+        bugs_page.is_bug_edited()
 
+    def test_delete_bug(self, browser):
+        """Deleting edited bug"""
+        bugs_page = BugsPage(browser, browser.current_url)
+        bugs_page.delete_bug()
+        bugs_page.is_bug_deleted()
--- Sanplex_project/test_crud_story.py
+++ Sanplex_project/test_crud_story.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from Sanplex_project.pages.base_page import BasePage
 from Sanplex_project.pages.project_page import ProjectPage
 from Sanplex_project.pages.dashboard_page import SideBar
@@ -12,33 +14,27 @@ PROJECT = "Next weird project 1"
 
 
 class TestCRUDStory:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        self.page = BasePage(browser, LINK)
+        self.page.open()
+        self.page.login(USER, PASSWORD)
+        self.project_page = ProjectPage(browser, browser.current_url)
+        self.project_page.should_this_project_page()
+        self.sidebar = SideBar(browser, browser.current_url)
+        self.sidebar.go_to_projects_list()
+        self.sidebar.go_to_project()
+        self.sidebar.should_title_project_correct()
+        self.sidebar.go_to_user_stories_list_from_sidebar()
 
     def test_create_story(self, browser):
-        page = BasePage(browser, LINK)
-        page.open()
-        page.login(USER, PASSWORD)
-        project_page = ProjectPage(browser, browser.current_url)
-        project_page.should_this_project_page()
-        sidebar = SideBar(browser, browser.current_url)
-        sidebar.go_to_projects_list()
-        sidebar.go_to_project()
-        sidebar.should_title_project_correct()
-        sidebar.go_to_user_stories_list_from_sidebar()
+        """Test creating a new story"""
         story = StoryPage(browser, browser.current_url)
         story.create_new_story()
         story.should_created_story_in_stories_list()
 
     def test_edit_story(self, browser):
-        page = BasePage(browser, LINK)
-        page.open()
-        page.login(USER, PASSWORD)
-        project_page = ProjectPage(browser, browser.current_url)
-        project_page.should_this_project_page()
-        sidebar = SideBar(browser, browser.current_url)
-        sidebar.go_to_projects_list()
-        sidebar.go_to_project()
-        sidebar.should_title_project_correct()
-        sidebar.go_to_user_stories_list_from_sidebar()
+        """Test editing created in a previous step story"""
         story = StoryPage(browser, browser.current_url)
         story.should_created_story_in_stories_list_for_edit()
         story.dropdown_story_box()
@@ -46,16 +42,7 @@ class TestCRUDStory:
         story.is_the_story_edited()
 
     def test_delete_story(self, browser):
-        page = BasePage(browser, LINK)
-        page.open()
-        page.login(USER, PASSWORD)
-        project_page = ProjectPage(browser, browser.current_url)
-        project_page.should_this_project_page()
-        sidebar = SideBar(browser, browser.current_url)
-        sidebar.go_to_projects_list()
-        sidebar.go_to_project()
-        sidebar.should_title_project_correct()
-        sidebar.go_to_user_stories_list_from_sidebar()
+        """Test deleting edited in a previous step story"""
         story = StoryPage(browser, browser.current_url)
         story.should_created_story_in_stories_list_for_edit()
         story.delete_story()
